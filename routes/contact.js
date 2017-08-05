b61ae656dbc185a4c6a046e9e67a1fcfe0b89363
Doug Hughes <doug@doughughes.net>
2017-08-05 11:21:32 -0400

    Can now delete contacts

--- routes/contact.js
+++ routes/contact.js
@@ -47,6 +47,27 @@ routes.get('/contactForm', (req, res) => {
     );
 });
 
+routes.get('/deleteContact', (req, res) => {
+  if (!req.session.userId) return res.redirect('/user/login');
+
+  // find the current user
+  User.findOne({ _id: req.session.userId })
+    // find the contact being edited, if any
+    .then(user => {
+      user.contacts
+        .id(req.query.id)
+        .remove()
+        .then(() => {
+          return user.save();
+        })
+        .then(() => res.redirect('/'));
+    });
+});
+
+/**
+ * creates or updates a contact
+ * @type {Contact}
+ */
 routes.post('/saveContact', (req, res) => {
   if (!req.session.userId) return res.redirect('/user/login');
 
@@ -87,51 +108,6 @@ routes.post('/saveContact', (req, res) => {
         });
       })
   );
-
-  // (req.body._id
-  //   ? User.findOneAndUpdate(
-  //       { _id: req.session.userId, 'contacts._id': req.body._id },
-  //       { 'contacts.$': contact },
-  //       { upsert: true, runValidators: true }
-  //     )
-  //   : User.findOneAndUpdate({ _id: req.session.userId }, { $push: { contacts: contact } }, { runValidators: true }))
-  //   .then(user => {
-  //     console.log('update', user);
-  //     res.send('sdffdsfsd');
-  //   })
-  //   .catch(err => {
-  //     res.render('contactForm', {
-  //       contact: contact,
-  //       err: err
-  //     });
-  //   });
-
-  // // get the user for the contact we are adding/editing
-  // User.findByIdAndUpdate(
-  //   req.session.userId,
-  //   { $push: { contacts: req.body } },
-  //   { safe: true, upsert: true, new: true }
-  // ).then(user => {
-  //   // get the contact geing edited or create a new contact
-  //   // const contact = user.contacts.id(req.body._id) || new Contact();
-  //   // contact.update(req.body);
-  //   console.log('??????');
-  //   res.send('fsddfsfdsfsd');
-  // const contact = user.contacts.id(req.body._id) || new Contact(req.body);
-  // console.log("edit?", user.contacts.id(req.body._id));
-  // contact
-  //   .validate()
-  //   .then(() => {
-  //     user.contacts.push(req.body);
-  //     return user.save();
-  //   })
-  //   .then(() => res.send('saved!!!'))
-  //   .catch(err => {
-  //     res.render('contactForm', {
-  //       contact: contact,
-  //       err: err
-  //     });
-  //   });
 });
 
 module.exports = routes;
